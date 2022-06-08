b6414f7f7379bf3acd1dbb47c11eb8d7ce23cd53
Xavi Aracil <xaracil@uoc.edu>
2022-06-08 13:22:07 +0200

    Added method deleteRubric(String courseId, String id)

--- src/main/java/edu/ksu/canvas/impl/RubricImpl.java
+++ src/main/java/edu/ksu/canvas/impl/RubricImpl.java
@@ -59,6 +59,14 @@ public class RubricImpl extends BaseImpl<Rubric, RubricReader, RubricWriter> imp
 	}
 
 	@Override
+	public Optional<Rubric> deleteRubric(String courseId, String id) throws IOException {
+		LOG.debug("deleting rubric {} in course {}", id, courseId);
+		String url = buildCanvasUrl("courses/" + courseId + "/rubrics/" + id, Collections.emptyMap());
+		Response response = canvasMessenger.deleteFromCanvas(oauthToken, url, Collections.emptyMap());
+		return responseParser.parseToObject(Rubric.class, response);
+	}
+
+	@Override
     protected Type listType() {
         return new TypeToken<List<Rubric>>(){}.getType();
     }
--- src/main/java/edu/ksu/canvas/interfaces/RubricWriter.java
+++ src/main/java/edu/ksu/canvas/interfaces/RubricWriter.java
@@ -18,4 +18,15 @@ public interface RubricWriter extends CanvasWriter<Rubric, RubricWriter> {
 	 * @throws IOException When there is an error communicating with Canvas
 	 */
 	Optional<RubricWriterResponse> createSingleRubricInCourse(String courseId, RubricCreationRequest rubric) throws IOException;
+
+	/**
+	 * Delete a single rubric
+	 * See <a href="https://aula.uoc.edu/doc/api/rubrics.html#method.rubrics.destroy">Delete a single rubric</a>
+	 * in the Canvas docs for details.
+	 * @param courseId the course ID of the course under which the rubric is places
+	 * @param id A rubric ID of the rubric to be deleted
+	 * @return deleted rubric object
+	 * @throws IOException When there is an error communicating with Canvas
+	 */
+	Optional<Rubric> deleteRubric(String courseId, String id) throws IOException;
 }
